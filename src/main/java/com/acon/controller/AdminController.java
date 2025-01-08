4711fba2c52457c8a06ffea2d82aae78b7f7415f
Son Castle <soncastle1090@gmail.com>
2025-01-08 23:14:15 +0900

    fetch방식을 통한 비동기식 비밀번호 확인 일치여부 추가

--- src/main/java/com/acon/controller/AdminController.java
+++ src/main/java/com/acon/controller/AdminController.java
@@ -1,15 +1,17 @@
 package com.acon.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.beans.Mergeable;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonWriter.Members;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -43,20 +45,40 @@ public class AdminController {
 //		model.addAttribute("msg", msg);
 	}
 	
+//	@PostMapping("/passwordConform")
+//	public void passwordConform(@RequestBody Map<String, String> request,
+//			@RequestParam("password") String password,
+//			@RequestParam("passwordConform") String passwordConform,
+//			Model model) {
+//		boolean result = memberService.passwordConform(password, passwordConform);
+//		if(result) {
+//			model.addAttribute("passwordConform", true);
+//			model.addAttribute("msg", "비밀번호가 일치합니다.");
+//		}
+//		else {
+//			model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
+//			model.addAttribute("msg", false);
+//		}
+//	}
+	
 	@PostMapping("/passwordConform")
-	public void passwordConform(@RequestParam("password") String password,
-							@RequestParam("passwordConform") String passwordConform,
-							Model model) {
-		boolean result = memberService.passwordConform(password, passwordConform);
-		if(result) {
-			model.addAttribute("passwordConform", true);
-			model.addAttribute("msg", "비밀번호가 일치합니다.");
-		}
-		else {
-			model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
-			model.addAttribute("msg", false);
-		}
-	}
+    public ResponseEntity<Map<String, Object>> validatePassword(@RequestBody Map<String, String> request) {
+        // 비밀번호 확인 (비밀번호 일치 여부만 체크)
+        String password = request.get("password");
+        String passwordConfirm = request.get("passwordConfirm");
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (password.equals(passwordConfirm)) {
+            response.put("success", true);
+            response.put("message", "비밀번호가 확인되었습니다.");
+        } else {
+            response.put("success", false);
+            response.put("message", "비밀번호가 일치하지 않습니다.");
+        }
+
+        return ResponseEntity.ok(response); // 결과 응답
+    }
 	
 	@GetMapping("/adminRegister")
 	public String adminRegister() {
